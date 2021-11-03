61e6c22f9b862bafa59cd8b68b87a6e2567ea184
mlees <mlees@LAPTOP-MK60QSQ2.attlocal.net>
2021-11-03 19:41:35 -0400

    Commit 03Nov21 19:41

--- src/com/smartdronelab/dronemodel/ElectronicSpeedController.java
+++ src/com/smartdronelab/dronemodel/ElectronicSpeedController.java
@@ -9,4 +9,16 @@ package com.smartdronelab.dronemodel;
  */
 public class ElectronicSpeedController extends Sensor {
 
+	@Override
+	public void measureData() {
+		// TODO Auto-generated method stub
+		super.measureData();
+	}
+
+	@Override
+	public void sendData() {
+		// TODO Auto-generated method stub
+		super.sendData();
+	}
+
 }
